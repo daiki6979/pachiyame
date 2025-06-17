1bd7141d7b752fe0d54fe28d423abd3af91c12b2
daiki6979 <147133589+daiki6979@users.noreply.github.com>
2025-06-17 12:42:07 +0900

    Delete pachiyame-1 directory

--- pachiyame-1/backend/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once('api/session_config.php');
-    session_unset();
-    session_destroy();
-    
-    header('Content-Type: application/json');
-    echo json_encode(['status' => 'ok']);
-    exit();
-?>
